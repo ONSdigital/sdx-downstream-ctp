57a9305661812876c8c3bd3921facf8a828ad10e
J Gregory <john@johngregory.me.uk>
2017-02-08 12:59:08 +0000

    Ensure tx_id in log messages

--- app/consumer.py
+++ app/consumer.py
@@ -43,16 +43,16 @@ class Consumer(AsyncConsumer):
         try:
             processor.process()
             self.acknowledge_message(basic_deliver.delivery_tag, tx_id=processor.tx_id)
-            logger.info("Processed successfully")
+            logger.info("Processed successfully", tx_id=processor.tx_id)
 
         except BadMessageError as e:
             # If it's a bad message then we have to reject it
             self.reject_message(basic_deliver.delivery_tag, tx_id=processor.tx_id)
-            logger.error("Bad message", action="rejected", exception=e, delivery_count=delivery_count)
+            logger.error("Bad message", action="rejected", exception=e, delivery_count=delivery_count, tx_id=processor.tx_id)
 
         except (RetryableError, Exception) as e:
             self.nack_message(basic_deliver.delivery_tag, tx_id=processor.tx_id)
-            logger.error("Failed to process", action="nack for retry", exception=e, delivery_count=delivery_count)
+            logger.error("Failed to process", action="nack for retry", exception=e, delivery_count=delivery_count, tx_id=processor.tx_id)
 
 
 def main():
