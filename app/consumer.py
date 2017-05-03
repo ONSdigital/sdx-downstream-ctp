eaee8c64e0b165860fdac964e188387bb4aafaa4
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2017-05-03 10:50:48 +0100

    Remove environment variable defaults

--- app/consumer.py
+++ app/consumer.py
@@ -6,6 +6,26 @@ from .processor import CTPProcessor
 from app import settings
 from app.helpers.sftp import SFTP
 from app.helpers.exceptions import BadMessageError, RetryableError
+import os
+import sys
+
+
+def check_default_env_vars():
+
+    env_vars = ["SDX_STORE_URL", "SDX_SEQUENCE_URL", "CTP_FTP_HOST", "CTP_FTP_USER",
+                "CTP_FTP_PASS", "CTP_FTP_FOLDER", "CTP_FTP_HEARTBEAT_FOLDER", "SFTP_HOST",
+                "SFTP_PORT", "SFTP_USER", "SFTP_PRIVATEKEY_FILENAME", "SFTP_PUBLICKEY_FILENAME",
+                "CTP_NOTIFICATIONS_QUEUE", "RABBITMQ_EXCHANGE", "RABBITMQ_HOST", "RABBITMQ_PORT",
+                "RABBITMQ_DEFAULT_USER", "RABBITMQ_DEFAULT_PASS", "RABBITMQ_DEFAULT_VHOST",
+                "RABBITMQ_HOST2", "RABBITMQ_PORT2"]
+
+    for i in env_vars:
+        if os.getenv(i) is None:
+            logger.error("No ", i, "env var supplied")
+            missing_env_var = True
+
+    if missing_env_var is True:
+        sys.exit(1)
 
 
 def get_delivery_count_from_properties(properties):
@@ -61,6 +81,7 @@ class Consumer(AsyncConsumer):
 
 def main():
     logger.info("Starting consumer", version=__version__)
+    check_default_env_vars()
     consumer = Consumer()
     try:
         consumer.run()
